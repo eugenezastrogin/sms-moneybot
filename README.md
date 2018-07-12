361de31f53c3bddb7142a71cf9b2fda61a4431fb
Eugene <aethelz@users.noreply.github.com>
2018-07-12 21:35:07 +0300

    'Proof of concept' commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # sms-moneybot
 SMS parsing telegram bot that keeps track of your salary
+
+What does it do:
+Keeps an SQLite DB with parsed data from SMS that you sent to the bot.
+
+They have to be of the following type (standard SBERBANK SMS):
+
+    VISA1234 21.12.16 22:12
+    зачисление зарплаты 12345.57р
+    Баланс: 16063.28р
+
+Offers multiple commands to get statistics on your salary.
+
+List of commands:
+/userdata - returns all user db records
+/userinfo - prints current chat_id and number of records in DB of a current user
+/wage MM YYYY (both arguments are optional, if none given simply returns last month's data)
+
+Admin commands:
+/alldata - returns all DB records
+/dumpdb - sends an ENTIRE database as a file
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from functools import wraps
-import telegram, logging
-import configparser, sqlite3, pathlib, re, datetime, signal, os
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import logging, configparser, sqlite3, re, datetime, signal, os
 
 # logging initialize
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -12,6 +12,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 # config initializing
 config = configparser.ConfigParser()
 config.read('config.ini')
+separator_date = int(config['DEFAULT']['separator_date'])
 
 # telegram bot initializing
 updater = Updater(token=config['DEFAULT']['token'])
@@ -21,7 +22,6 @@ dispatcher = updater.dispatcher
 # SQLite initializing
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 db_path = os.path.join(BASE_DIR, "data.db")
-p = pathlib.Path(db_path)
 
 # Generic functions
 
@@ -41,7 +41,9 @@ def parse_sms(text: str) -> dict:
     \ +                                    # separator is one space or more
     (\d{2}\.\d{2}\.\d{2}\ \d{2}\:\d{2})    # matching date and time
     \D*                       # optional separator is any number of non digits
-    (\d+\.\d+)                             # matching amount
+    зарплаты
+    \D*                       # optional separator is any number of non digits
+    (\d+\.*\d*)                             # matching amount
     ''', re.VERBOSE)
 
     parsed_sms = sms_pattern.search(text).groups()
@@ -51,24 +53,22 @@ def parse_sms(text: str) -> dict:
 
     return sms_data
 
-
 # SQLite retrieval functions
+
 
 def datatable_init():
     """
-    creates if necessary and opens data.db SQLite database
+    creates if necessary data.db SQLite database
     """
-    with sqlite3.connect(db_path, check_same_thread=False) as db:
-        cursor = db.cursor()
-        if not p.is_file():
-            cursor.execute('''CREATE TABLE data (
-                       chat_id INTEGER,
-                       name TEXT,
-                       card TEXT,
-                       date_time DATETIME,
-                       amount REAL
-                       )''')
-            db.commit()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS data (
+               chat_id INTEGER,
+               name TEXT,
+               card TEXT,
+               date_time DATETIME,
+               amount REAL,
+               UNIQUE(chat_id, name, card, date_time, amount)
+               )''')
+    db.commit()
 
 
 def insert_transaction(chat_id: int, username: str, sms_data: dict):
@@ -76,7 +76,7 @@ def insert_transaction(chat_id: int, username: str, sms_data: dict):
     takes in chat_id of the convo and parsed sms dictionary, writes data into DB
     """
     with db:
-        cursor.execute("INSERT INTO data VALUES (:id, :name, :card, :datetime, :amount)",
+        cursor.execute("INSERT OR IGNORE INTO data VALUES (:id, :name, :card, :datetime, :amount)",
                        {'id': chat_id, 'name': username, 'card': sms_data['card'],
                         'datetime': sms_data['datetime'], 'amount': sms_data['amount']})
 
@@ -97,6 +97,15 @@ def user_data(chat_id: str) -> list:
     return cursor.fetchall()
 
 
+def new_card(chat_id: str, card: str) -> bool:
+    """
+    checks whether this card type and number already in DB for this user
+    """
+    cursor.execute("SELECT COUNT(*) FROM data WHERE chat_id=:id AND card=:card",
+            {'id': chat_id, 'card': card})
+    return False if cursor.fetchone()[0] > 0 else True
+
+
 def user_records(chat_id: str) -> list:
     """
     returns number of relevant user records
@@ -105,12 +114,17 @@ def user_records(chat_id: str) -> list:
     return cursor.fetchone()[0]
 
 
-def test_sum(chat_id: str) -> list:
-    cursor.execute("SELECT SUM(amount) FROM data WHERE chat_id=:id", {'id': chat_id})
+def wage_calc(chat_id: str, dates: dict) -> list:
+    cursor.execute("SELECT SUM(amount) FROM data WHERE chat_id=:id \
+                      AND date_time BETWEEN ':sy-:sm-:sd' and ':ey-:em-:ed'",
+                      {'id': chat_id, 'sy': dates['sy'], 'sm': dates['sm'],
+                       'sd': dates['sd'], 'ey': dates['ey'], 'em': dates['sy'],
+                       'ed': dates['ed']})
     return cursor.fetchone()
 
 
 # Bot handlers
+
 
 def restricted(func):
     @wraps(func)
@@ -132,17 +146,52 @@ def start(bot, update):
 
 def sms(bot, update):
     """
-    on receiving SMS, tries to parse it and add to DB, otherwise reprompts user
+    on receiving SMS, tries to parse it and add to DB, otherwise reprompts user.
+    Also shows accumulative wage in a month that SMS was from
     """
     try:
         sms = parse_sms(update.message.text)
-        insert_transaction(update.message.chat_id, update.message.from_user['username'], sms)
-        bot.send_message(chat_id=update.message.chat_id,
-                         text='Transaction added successfully')
-    except Exception as e:
-        print(e)
+    except AttributeError:
         bot.send_message(chat_id=update.message.chat_id,
                          text='Unable to parse. Please, send valid SMS!')
+        return None
+
+    # EXPERIMANTAL MENU PART
+
+#   if new_card(update.message.chat_id, sms['card']):
+#       bot.send_message(chat_id=update.message.chat_id,
+#            text="I don't have any records with this particular card number.")
+
+#       button_list = [InlineKeyboardButton("Yes, add it.", callback_data="1"),
+#                      InlineKeyboardButton("No, trash it!", callback_data="0")]
+#       reply_markup = InlineKeyboardMarkup(button_list)
+
+#       update.message.reply_text("Are you sure everything is correct?",
+#                        reply_markup=reply_markup)
+
+    # ENF OF MENU
+
+
+    insert_transaction(update.message.chat_id, update.message.from_user['username'], sms)
+    bot.send_message(chat_id=update.message.chat_id,
+                     text='Transaction added successfully!')
+
+    if sms['datetime'].date() == datetime.datetime.now().date():
+        now = datetime.datetime.now()
+        month = now.month if now.day > separator_date else now.month - 1
+        dates = {'sy': now.year, 'sm': month-1, 'sd': separator_date, 'ey': now.year,
+                 'em': month, 'ed': separator_date}
+        wage = wage_calc(update.message.chat_id, dates)
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Your last month's wage is {} so far".format(wage))
+    else:
+        now = sms['datetime']
+        month = now.month if now.day > separator_date else now.month - 1
+        dates = {'sy': now.year, 'sm': month-1, 'sd': separator_date, 'ey': now.year,
+                 'em': month, 'ed': separator_date}
+        wage = wage_calc(update.message.chat_id, dates)
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Your wage in that month is {} so far".format(wage))
 
 
 @restricted
@@ -161,13 +210,48 @@ def user_data(bot, update):
                      text=str(user_data(update.message.chat_id)))
 
 
+def wage_request(bot, update, args):
+    ''' takes arguments, tries to parse them and return wage for the mentioned
+    period
+    for now: 06 2017'''
+    if len(args) == 0:
+        now = datetime.datetime.now()
+        month = now.month if now.day > separator_date else now.month - 1
+        dates = {'sy': now.year, 'sm': month-1, 'sd': separator_date, 'ey': now.year,
+                 'em': month, 'ed': separator_date}
+    elif len(args) == 1:
+        if len(args[0]) > 2:
+            #enitire year
+            pass
+        else:
+            month = args[0]
+            year = datetime.datetime.now().year
+            month = now.month if now.day > separator_date else now.month - 1
+            dates = {'sy': year, 'sm': month-1, 'sd': separator_date,
+                     'ey': year, 'em': month, 'ed': separator_date}
+    else:
+        month = args[0]
+        year = args[1]
+        dates = {'sy': year, 'sm': month-1, 'sd': separator_date, 'ey': year,
+                 'em': month, 'ed': separator_date}
+
+    wage = wage_calc(update.message.chat_id, dates)
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="Your wage in that period was {}".format(wage))
+
+
 def user_info(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
                  text="Your chat_id is {}, we have {} records concerning you".\
                  format(update.message.chat_id, user_records(update.message.chat_id)))
 
 
+def unknown(bot, update):
+    bot.send_message(chat_id=updater.message.chat_id,
+                     text="Unknown command.")
+
 # MAIN LOOP HERE
+
 
 def main():
     start_handler = CommandHandler('start', start)
@@ -175,7 +259,10 @@ def main():
     userdata_handler = CommandHandler('userdata', user_data)
     dumpdb_handler = CommandHandler('dumpdb', dump_db)
     userinfo_handler = CommandHandler('userinfo', user_info)
+    wagerequest_handler = CommandHandler('wage', wage_request, pass_args=True)
     sms_handler = MessageHandler(Filters.text, sms)
+#   newcardmenu_handler = CallbackQueryHandler(newcardmenu))
+    unknown_handler = MessageHandler(Filters.command, unknown)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(alldata_handler)
@@ -183,10 +270,22 @@ def main():
     dispatcher.add_handler(dumpdb_handler)
     dispatcher.add_handler(userinfo_handler)
     dispatcher.add_handler(sms_handler)
+    dispatcher.add_handler(wagerequest_handler)
+#   dispatcher.add_handler(newcardmenu_handler)
+    #unknown handler should go last!
+    dispatcher.add_handler(unknown_handler)
 
     updater.start_polling()
 
 
 if __name__ == '__main__':
+    with sqlite3.connect(db_path, check_same_thread=False) as db:
+        cursor = db.cursor()
     datatable_init()
     main()
+else:
+    # Don't mess with main DB if imported as a module
+    db_path = os.path.join(BASE_DIR, "test.db")
+    with sqlite3.connect(db_path, check_same_thread=False) as db:
+        cursor = db.cursor()
+    datatable_init()
